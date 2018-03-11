5aacff0dd1e711eb76b73c239fdb758ef0a59557
Armen Boursalian <aboursalian@gmail.com>
2018-03-10 20:09:14 -0800

    Main application compiles; syntax err in valid rule

--- yara-parser.go
+++ yara-parser.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"yara-parser/grammar"
+)
 
+func main() {
+	input, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatalf("Error: %s\n", err)
+	}
+
+	result := grammar.Parse(input, os.Stdout)
+	log.Printf("Result: %d\n", result)
+
+	log.Printf("RuleSet: %v\n", grammar.ParsedRuleset)
 }
