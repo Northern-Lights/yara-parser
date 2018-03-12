f0139f3dcf6c1b9fbf185811435ce817f98f7c36
Armen Boursalian <aboursalian@gmail.com>
2018-03-11 18:17:41 -0700

    Modify data structures

--- data/data.go
+++ data/data.go
@@ -1,16 +1,5 @@
 package data
 
-func init() {
-	ResetRuleSet()
-}
-
-var rs *RuleSet
-
-// ResetRuleSet resets the global ruleset so the parser can parse a new ruleset
-func ResetRuleSet() {
-	rs = &RuleSet{}
-}
-
 // RuleSet represents the contents of a yara file
 type RuleSet struct {
 	Modules   []string
@@ -21,21 +10,30 @@ type RuleSet struct {
 
 // A Rule is a single yara rule
 type Rule struct {
-	Global     bool
-	Private    bool
+	Modifiers  RuleModifiers
 	Identifier string
 	Tags       []string
-	Meta       map[string]interface{}
+	Meta       Metas
 	Strings    map[string]*String
 	Condition  string
 }
+
+// RuleModifiers denote whether a Rule is global, private, neither, or both.
+type RuleModifiers struct {
+	Global  bool
+	Private bool
+}
+
+// Metas is a map which should only be used with values of type
+// int, float (64 or 32?), string, bool.
+type Metas map[string]interface{}
 
 // String is a string, regex, or byte pair sequence
 type String struct {
 	ID        string
 	Type      StringType
 	Text      string
-	Modifiers []StringModifier
+	Modifiers StringModifiers
 }
 
 // StringType is used to differentiate between string, hex bytes, and regex
@@ -48,14 +46,12 @@ const (
 	TypeRegex
 )
 
-// StringModifier is a modifiers to a string identifier
-type StringModifier int
-
-// Modifiers related to String type
-const (
-	StringModNocase StringModifier = iota
-	StringModASCII
-	StringModWide
-	RegexModI
-	RegexModS
-)
+// StringModifiers denote the status of the possible modifiers for strings
+type StringModifiers struct {
+	Nocase   bool
+	ASCII    bool
+	Wide     bool
+	Fullword bool
+	I        bool // for regex
+	S        bool // for regex
+}
