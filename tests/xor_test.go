b3d4443202d39caf733942f87538560fbb6b6300
Armen Boursalian <aboursalian@aol.com>
2020-04-06 20:50:58 -0700

    add xor serialization tests

--- tests/xor_test.go
+++ tests/xor_test.go
@@ -1,8 +1,11 @@
 package tests
 
 import (
+	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/Northern-Lights/yara-parser/data"
 )
 
 var xorBadRangeTests = map[string]string{
@@ -34,5 +37,109 @@ condition:
 			t.Errorf(`rule %s failed with "%s"; should have failed with "%s"`,
 				ruleName, err, data.ErrInvalidStringModifierCombo)
 		}*/
+	}
+}
+
+func TestBadXorSerialization(t *testing.T) {
+	rules := []data.Rule{
+		data.Rule{
+			Identifier: "XOR_NEGATIVE_ARG",
+			Strings: data.Strings{
+				data.String{
+					Type: data.TypeString,
+					ID:   "$s1",
+					Text: "abcdefg",
+					Modifiers: data.StringModifiers{
+						Xor: data.Xor{data.Dec(-1)},
+					},
+				},
+			},
+			Condition: "$s1",
+		},
+
+		data.Rule{
+			Identifier: "XOR_REVERSED_RANGE",
+			Strings: data.Strings{
+				data.String{
+					Type: data.TypeString,
+					ID:   "$s1",
+					Text: "abcdefg",
+					Modifiers: data.StringModifiers{
+						Xor: data.Xor{data.Dec(123), data.Dec(1)},
+					},
+				},
+			},
+			Condition: "$s1",
+		},
+
+		data.Rule{
+			Identifier: "XOR_OUT_OF_RANGE1",
+			Strings: data.Strings{
+				data.String{
+					Type: data.TypeString,
+					ID:   "$s1",
+					Text: "abcdefg",
+					Modifiers: data.StringModifiers{
+						Xor: data.Xor{data.Hex(0x100)},
+					},
+				},
+			},
+			Condition: "$s1",
+		},
+
+		data.Rule{
+			Identifier: "XOR_OUT_OF_RANGE2",
+			Strings: data.Strings{
+				data.String{
+					Type: data.TypeString,
+					ID:   "$s1",
+					Text: "abcdefg",
+					Modifiers: data.StringModifiers{
+						Xor: data.Xor{data.Hex(0x10), data.Hex(0x101)},
+					},
+				},
+			},
+			Condition: "$s1",
+		},
+
+		data.Rule{
+			Identifier: "XOR_WITH_REGEX",
+			Strings: data.Strings{
+				data.String{
+					Type: data.TypeRegex,
+					ID:   "$s1",
+					Text: "abcdefg",
+					Modifiers: data.StringModifiers{
+						Xor: data.Xor{},
+					},
+				},
+			},
+			Condition: "$s1",
+		},
+
+		data.Rule{
+			Identifier: "XOR_WITH_HEXPAIRS",
+			Strings: data.Strings{
+				data.String{
+					Type: data.TypeHexString,
+					ID:   "$s1",
+					Text: "ab cd ef",
+					Modifiers: data.StringModifiers{
+						Xor: data.Xor{},
+					},
+				},
+			},
+			Condition: "$s1",
+		},
+	}
+
+	for _, rule := range rules {
+		s, err := rule.Serialize()
+		if err == nil {
+			t.Errorf(`%s successfully serialized: %s`, rule.Identifier, s)
+		} else if !errors.Is(err, data.ErrInvalidStringModifierCombo) {
+			t.Errorf(`%s failed with "%s"; expected to fail with "%s"`,
+				rule.Identifier, err, data.ErrInvalidStringModifierCombo)
+		}
 	}
 }
